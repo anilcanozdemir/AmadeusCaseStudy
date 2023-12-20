69b47fc86ed7f564a610d7228dcdab797eb9ab0e
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-20 15:39:12 +0300

    Swagger configurations are created.

--- src/main/java/com/amadeus/amadeuscasestudy/AmadeusCaseStudyApplication.java
+++ src/main/java/com/amadeus/amadeuscasestudy/AmadeusCaseStudyApplication.java
@@ -1,9 +1,13 @@
 package com.amadeus.amadeuscasestudy;
 
+import com.amadeus.amadeuscasestudy.Controller.FlightController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AmadeusCaseStudyApplication {
 
     public static void main(String[] args) {
