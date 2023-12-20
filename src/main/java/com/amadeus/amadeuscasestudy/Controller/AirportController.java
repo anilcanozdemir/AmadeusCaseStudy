407f383abe1377ab334a26130927c2d567802ada
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-20 16:52:39 +0300

    Controllers' methods are implemented.

--- src/main/java/com/amadeus/amadeuscasestudy/Controller/AirportController.java
+++ src/main/java/com/amadeus/amadeuscasestudy/Controller/AirportController.java
@@ -4,6 +4,9 @@ import com.amadeus.amadeuscasestudy.Core.Result.DataResult;
 import com.amadeus.amadeuscasestudy.Core.Result.Result;
 import com.amadeus.amadeuscasestudy.DTO.Airport.AirportDTO;
 import com.amadeus.amadeuscasestudy.DTO.Airport.AirportSaveRequestDTO;
+import com.amadeus.amadeuscasestudy.Service.Contract.AirportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,30 +14,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/airport")
+@RequiredArgsConstructor
 public class AirportController implements CRUDController<AirportDTO, AirportSaveRequestDTO>{
+    private AirportService airportService;
     @Override
     @PostMapping("/add")
     public ResponseEntity<Result> add(@RequestBody AirportSaveRequestDTO airportSaveRequestDTO) {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(airportService.add(airportSaveRequestDTO));
     }
     @GetMapping("/getAll")
     @Override
     public ResponseEntity<DataResult<List<AirportDTO>>> getAll() {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(airportService.getAll());
     }
     @GetMapping("/getById")
     @Override
     public ResponseEntity<DataResult<AirportDTO>> getById(@RequestParam Long id) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(airportService.getById(id));
+
     }
     @PostMapping("/updateById")
     @Override
     public ResponseEntity<Result> updateById(@RequestBody AirportDTO entityResponseDto) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(airportService.updateById(entityResponseDto));
     }
     @DeleteMapping("/deleteById")
     @Override
     public ResponseEntity<DataResult<AirportDTO>> deleteById(@RequestParam Long id) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(airportService.deleteById(id));
     }
 }
--- src/main/java/com/amadeus/amadeuscasestudy/Controller/FlightController.java
+++ src/main/java/com/amadeus/amadeuscasestudy/Controller/FlightController.java
@@ -4,6 +4,9 @@ import com.amadeus.amadeuscasestudy.Core.Result.DataResult;
 import com.amadeus.amadeuscasestudy.Core.Result.Result;
 import com.amadeus.amadeuscasestudy.DTO.Flight.FlightDTO;
 import com.amadeus.amadeuscasestudy.DTO.Flight.FlightSaveRequestDTO;
+import com.amadeus.amadeuscasestudy.Service.Contract.FlightService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +14,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flight")
+@RequiredArgsConstructor
 public class FlightController implements CRUDController<FlightDTO, FlightSaveRequestDTO> {
+    private FlightService flightService;
+
     @PostMapping("/add")
     @Override
     public ResponseEntity<Result> add(@RequestBody FlightSaveRequestDTO flightSaveRequestDTO) {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(flightService.add(flightSaveRequestDTO));
     }
     @GetMapping("/getAll")
     @Override
     public ResponseEntity<DataResult<List<FlightDTO>>> getAll() {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(flightService.getAll());
     }
     @GetMapping("/getById")
     @Override
     public ResponseEntity<DataResult<FlightDTO>> getById(@RequestParam Long id) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(flightService.getById(id));
     }
 
     @PostMapping("/updateById")
     @Override
     public ResponseEntity<Result> updateById(@RequestBody FlightDTO entityResponseDto) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(flightService.updateById(entityResponseDto));
     }
     @DeleteMapping("/deleteById")
     @Override
     public ResponseEntity<DataResult<FlightDTO>> deleteById(@RequestParam Long id) {
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(flightService.deleteById(id));
     }
 }
