5db9618a2bac55c8438288b56a87005e0842edb6
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-21 17:22:01 +0300

    ExceptionHandler created.

---
+++ src/main/java/com/amadeus/amadeuscasestudy/ExceptionHandling/ExceptionHandlerConfig.java
@@ -0,0 +1,23 @@
+package com.amadeus.amadeuscasestudy.ExceptionHandling;
+
+
+import com.amadeus.amadeuscasestudy.Core.Exception.EntityListEmptyException;
+import com.amadeus.amadeuscasestudy.Core.Exception.EntityNotFoundException;
+import com.amadeus.amadeuscasestudy.Core.Result.ErrorResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerConfig {
+    @ExceptionHandler({EntityListEmptyException.class})
+    public ResponseEntity<ErrorResult> handleEntityListEmptyException(EntityListEmptyException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResult(exception.getMessage()));
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<ErrorResult> handleEntityListEmptyException(EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResult(exception.getMessage()));
+    }
+}
